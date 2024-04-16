d3b58f2c299842b394e54e42a7a1b89643ee5e5d
degull <skytmdgus99@gmail.com>
2024-04-16 23:29:10 +0900

    detail page 재정렬

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -29,49 +29,37 @@ const Detail = () => {
     }
   };
 
+  const handleProductClick = (productPrice) => {
+    handleProductSelection(productPrice);
+  };
+
   return (
     <S.Wrapper>
-      <div>
-        {/* 잔돈 표시 */}
-        <span>투입: {moneyInput}원</span><br />
-        <span>거스름돈: {changeOutput}원</span>
-      </div>
-
-      <div>
+      <S.MoneyInputContainer>
         {/* 화폐 입력 버튼 */}
         {currencyUnits.map((unit) => (
           <button key={unit} onClick={() => handleMoneyInput(unit)}>
             {unit}원
           </button>
         ))}
-      </div>
-
-      <div>
-        {/* 음료 선택 영역 */}
-        <button onClick={() => handleProductSelection(450)}>물 (450원)</button>
-        <button onClick={() => handleProductSelection(500)}>커피 (500원)</button>
-        <button onClick={() => handleProductSelection(550)}>이온음료 (550원)</button>
-        <button onClick={() => handleProductSelection(550)}>고급커피 (700원)</button>
-        <button onClick={() => handleProductSelection(550)}>탄산음료 (750원)</button>
-        <button onClick={() => handleProductSelection(550)}>특화음료 (800원)</button>
-      </div>
+      </S.MoneyInputContainer>
 
       <S.Line1 />
       <S.TextImageContainer1>
         {/* 물 */}
-        <S.WaterGroup>
+        <S.WaterGroup onClick={() => handleProductClick(450)}>
           <S.WaterImage src="/img/water.png" alt="Water" />
           <S.WaterText>450원</S.WaterText>
         </S.WaterGroup>
 
         {/* 커피 */}
-        <S.CoffeeGroup>
+        <S.CoffeeGroup onClick={() => handleProductClick(500)}>
           <S.CoffeeImage src="/img/coffee.png" alt="Coffee" />
           <S.CoffeeText>500원</S.CoffeeText>
         </S.CoffeeGroup>
 
         {/* 이온음료 */}
-        <S.SportsDrinkGroup>
+        <S.SportsDrinkGroup onClick={() => handleProductClick(550)}>
           <S.SportsDrinkImage src="/img/sports_drink.png" alt="Sports Drink" />
           <S.SportsDrinkText>550원</S.SportsDrinkText>
         </S.SportsDrinkGroup>
@@ -80,23 +68,29 @@ const Detail = () => {
       <S.Line2 />
       <S.TextImageContainer2>
         {/* 고급커피 */}
-        <S.CoffeeStarGroup>
+        <S.CoffeeStarGroup onClick={() => handleProductClick(700)}>
           <S.CoffeeStarImage src="/img/coffee_star.png" alt="Water" />
           <S.CoffeeStarText>700원</S.CoffeeStarText>
         </S.CoffeeStarGroup>
 
         {/* 탄산음료 */}
-        <S.SparklingGroup>
+        <S.SparklingGroup onClick={() => handleProductClick(750)}>
           <S.SparklingImage src="/img/sparkling.png" alt="Water" />
           <S.SparklingText>750원</S.SparklingText>
         </S.SparklingGroup>
 
         {/* 특화음료 */}
-        <S.SpecialGroup>
+        <S.SpecialGroup onClick={() => handleProductClick(800)}>
           <S.SpecialImage src="/img/special.png" alt="Water" />
           <S.SpecialText>800원</S.SpecialText>
         </S.SpecialGroup>
       </S.TextImageContainer2>
+
+      <div>
+        {/* 잔돈 표시 */}
+        <span>투입: {moneyInput}원</span><br />
+        <span>거스름돈: {changeOutput}원</span>
+      </div>
     </S.Wrapper>
   );
 };
--- src/components/Detail/Detail.styled.js
+++ src/components/Detail/Detail.styled.js
@@ -1,25 +1,29 @@
-// Detail.styled.js
 import styled from "@emotion/styled";
+
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
+  min-height: 80vh;
+  position: relative;
 `;
 
 export const Line1 = styled.div`
   width: 300px;
   border-bottom: 10px solid gray;
   border-radius: 10px;
-  margin-top: 50px;
-  margin: 20px 0;
+  margin: 0 0 -50px 0;
+  
 `;
 
 export const Line2 = styled.div`
   width: 300px;
   border-bottom: 10px solid gray;
   border-radius: 10px;
-  margin: 20px 0;
+  margin-top: 80px;
+  margin-bottom: -50px;
 `;
 
 export const TextImageContainer1 = styled.div`
@@ -74,64 +78,123 @@ export const WaterImage = styled.img`
   width: 30%;
   height: auto;
   cursor: pointer;
+  transition: transform 0.3s ease;
+
+&:hover {
+  transform: scale(1.1);
+}
 `;
 
 export const CoffeeImage = styled.img`
   width: 30%;
   height: auto;
   cursor: pointer;
+  transition: transform 0.3s ease;
+
+&:hover {
+  transform: scale(1.1);
+}
 `;
 
 export const SportsDrinkImage = styled.img`
   width: 30%;
   height: auto;
   cursor: pointer;
+  transition: transform 0.3s ease;
+
+&:hover {
+  transform: scale(1.1);
+}
 `;
 
 export const CoffeeStarImage = styled.img`
   width: 30%;
   height: auto;
   cursor: pointer;
+  transition: transform 0.3s ease;
+
+&:hover {
+  transform: scale(1.1);
+}
 `;
 
 export const SparklingImage = styled.img`
   width: 30%;
   height: auto;
   cursor: pointer;
+  transition: transform 0.3s ease;
+
+&:hover {
+  transform: scale(1.1);
+}
 `;
 
 export const SpecialImage = styled.img`
   width: 30%;
   height: auto;
   cursor: pointer;
+  transition: transform 0.3s ease;
+
+&:hover {
+  transform: scale(1.1);
+}
 `;
 
 export const WaterText = styled.div`
   font-size: 12px;
   font-weight: bold;
+  margin-top: 30px;
 `;
 
 export const CoffeeText = styled.div`
   font-size: 12px;
   font-weight: bold;
+  margin-top: 30px;
 `;
 
 export const SportsDrinkText = styled.div`
   font-size: 12px;
   font-weight: bold;
+  margin-top: 30px;
 `;
 
 export const CoffeeStarText = styled.div`
   font-size: 12px;
   font-weight: bold;
+  margin-top: 30px;
 `;
 
 export const SparklingText = styled.div`
   font-size: 12px;
   font-weight: bold;
+  margin-top: 30px;
 `;
 
 export const SpecialText = styled.div`
   font-size: 12px;
   font-weight: bold;
+  margin-top: 30px;
+`;
+
+
+export const MoneyInputContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 30px 0 70px 0;
+
+  button {
+    background-color: #4caf50;
+    color: white;
+    padding: 10px 10px;
+    margin: 0 5px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 12px;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+      background-color: #45a049;
+    }
+  }
 `;
